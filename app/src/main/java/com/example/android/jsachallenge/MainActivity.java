2ad441f3774e40f5c99d2867b2e915ccc88cefc8
Natalia <idmitrievanm@gmail.com>
2019-07-01 12:19:10 +1200

    Networking. Retrofit library and Retrofit Jackson were used to create a request and convert Json into Java objects. Created Repo class and GitHubService Interface. Implemented Retrofit functionality in the main activity.

--- app/src/main/java/com/example/android/jsachallenge/MainActivity.java
+++ app/src/main/java/com/example/android/jsachallenge/MainActivity.java
@@ -1,25 +1,40 @@
 package com.example.android.jsachallenge;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
     Button fetchButton;
     EditText userNameEditText;
+    TextView textView;
+
+    Retrofit mRetrofit;
+    GitHubService mService;
+    List<Repo> mRepos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.setTitle("Git User Repo List");
 
         //binding up the button and the editText from the layout
-        fetchButton = (Button) findViewById(R.id.fetchButton);
-        userNameEditText = (EditText) findViewById(R.id.userName);
+        fetchButton = findViewById(R.id.fetchButton);
+        userNameEditText = findViewById(R.id.userName);
+        textView = findViewById(R.id.textView);
 
         fetchButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,8 +45,42 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        //instance of Retrofit
+        mRetrofit = new Retrofit.Builder()
+                .baseUrl("https://api.github.com/")
+                .addConverterFactory(JacksonConverterFactory.create())
+                .build();
+
+        //implementation of the GitHubService interface
+        mService = mRetrofit.create(GitHubService.class);
     }
 
     protected void fetchRepos(String user) {
+        mService.listRepos(user).enqueue(new Callback<List<Repo>>() {
+            @Override
+            public void onResponse(@NonNull Call<List<Repo>> call, @NonNull Response<List<Repo>> response) {
+                if (!response.isSuccessful()) {
+                    textView.setText(response.code());
+                }
+
+                mRepos = response.body();
+
+                assert mRepos != null;
+                for (Repo repo : mRepos) {
+                    String content = "";
+                    content += "Name: " + repo.getName() + "\n";
+                    content += "Description: " + repo.getDescription() + "\n";
+                    content += "HTML URL: : " + repo.getHtml_url() + "\n";
+
+                    textView.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<List<Repo>> call, @NonNull Throwable t) {
+                textView.setText(t.getMessage());
+            }
+        });
     }
 }
